beee0acd37a1eb03d40e45944a1bd227f13b9123
Seda Malkoç <sedaamalkocc@gmail.com>
2024-03-02 22:42:28 +0300

    seda Member section

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -1,5 +1,43 @@
+import { Button, Table } from "antd";
+import PageTitle from "../../components/PageTitle/PageTitle";
+import { useNavigate } from "react-router-dom";
+
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { MemberData } from "../../utils/mock/members";
+import { MemberTableHeaders } from "../../utils/data/members";
+
 const Members = () => {
-  return <div>Members</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <PageTitle title="Üyeler Tablosu" />
+      <div className="flex justify-end">
+        <Button onClick={() => navigate("new")}>Üye Ekle</Button>
+      </div>
+      <Table
+        columns={[
+          ...MemberTableHeaders,
+          {
+            title: "İşlemler",
+            dataIndex: "actions",
+            key: "actions",
+            render: () => (
+              <div className="flex gap-4 h-12 text-xl">
+                <EditOutlined
+                  className="hover:text-blue-500"
+                  onClick={() => navigate(`${3}/edit`)}
+                />
+                <DeleteOutlined className="hover:text-blue-500" />
+              </div>
+            ),
+          },
+        ]}
+        dataSource={MemberData}
+        className="pt-12"
+      />
+    </div>
+  );
 };
 
 export default Members;
